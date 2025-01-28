bfe424e6c773fadb7bc51c3325570250edcc8a56
Manisha7477 <mymanisha860@gmail.com>
2025-01-29 00:20:19 +0530

    dynamic enter value in routs

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 //middleware
-app.use(function(req, res, next){
-  console.log("Hey Hello!");
-  next(); //to go next function 
-})
+// app.use(function(req, res, next){
+//   console.log("Hey Hello!");
+//   next(); //to go next function 
+// })
 
 app.get('/', function (req, res) {
   res.send('Something about main page')
@@ -15,13 +15,19 @@ app.get('/about', function(req, res){
     res.send("Something about me!")
 })
 
-//dynamic routing
-app.get('/about/:username', function(req, res){
-  res.send("Something about username!")
-})
-//in every page ill show this when use *, called universal route
+//in every page will show this when use *, called universal route
 // app.get('*', function(req, res){
 //     res.send("Something star me!")
 // })
+
+//dynamic routing ..instaed of username we can write anything , doesn't matter
+// app.get('/profile/:username', function(req, res){
+//   res.send(req.params.username + " 's page");
+// })
+
+app.get("/author/:username/:age", function(req, res){
+  res.send("username:" + req.params.username + " " + " age:" + req.params.age )
+})
+
 
 app.listen(3000)
